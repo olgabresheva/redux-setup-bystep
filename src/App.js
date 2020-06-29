8ff0d02585eef805ea71cbcba519bd3bf5e666f5
olgab <olgabresheva7@gmail.com>
2020-06-28 20:00:08 -0700

    read data from redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import {connect} from "react-redux";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
      React App
+        <ul>
+            {
+                props.todos.map(el => <li key={el.title}>
+                    {el.title}
+                    {el.done ?  <button>Done</button> : null}
+                </li>)
+            }
+        </ul>
+
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+    todos: state.todos
+});
+
+export default connect(mapStateToProps) (App);
